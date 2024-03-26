886ec0e6284d4c699fc836ad334564fc9daf7d9f
AsandaMbangata <mbangataa9@gmail.com>
2024-03-26 23:57:41 +0200

    BookTicket POJO

--- src/main/java/za/ac/cput/domain/Main.java
+++ src/main/java/za/ac/cput/domain/Main.java
@@ -4,7 +4,13 @@ package za.ac.cput.domain;
     Author: Anele Nqabeni (220403635)
     Date: 17 March 2024
     */
+import za.ac.cput.factory.BookTicketFactory;
 import za.ac.cput.factory.MovieFactory;
+import za.ac.cput.repository.BookTicketRepository;
+import za.ac.cput.repository.IBookTicketRepository;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +22,20 @@ public class Main {
 
         Movie movie2 = MovieFactory.createMovie("3", "John Wick 4", "Keanu Reeves", 2022, "Action", 210);
         printMovie(movie2);
+
+        BookTicket bookTicket1 = BookTicketFactory.createBookTicket("BFW-ZTRYT-213", "John Wick 4", "Daniella Gilbert", LocalDateTime.parse("2024-04-13T18:30"), "B17");
+        BookTicket bookTicket2 = BookTicketFactory.createBookTicket("XLT-FLBSW-887", "Lift", "Jonathan Ford", LocalDateTime.parse("2024-04-26T20:30"), "F50");
+        BookTicket bookTicket3 = BookTicketFactory.createBookTicket("SGB-MBWPA-652","Fast and Furious 8", "Sydney April", LocalDateTime.parse("2024-04-20T20:00"), "A05");
+
+        IBookTicketRepository bookTicketRepository = BookTicketRepository.getRepository();
+        bookTicketRepository.create(bookTicket1);
+        bookTicketRepository.create(bookTicket2);
+        bookTicketRepository.create(bookTicket3);
+
+        Set<BookTicket> allBookTickets = bookTicketRepository.getAll();
+        for (BookTicket bookTicket : allBookTickets) {
+            System.out.println(bookTicket);
+        }
     }
 
     private static void printMovie(Movie movie) {
@@ -26,4 +46,3 @@ public class Main {
         }
     }
 }
-
---
+++ src/main/java/za/ac/cput/repository/BookTicketRepository.java
@@ -0,0 +1,62 @@
+package za.ac.cput.repository;
+
+/* BookTicket.java
+BookTicket model class
+Author: Asanda Mbangata (222927259)
+Date: 21 March 2024
+*/
+
+import za.ac.cput.domain.BookTicket;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class BookTicketRepository implements IBookTicketRepository {
+    private static BookTicketRepository repository = null;
+    private Set<BookTicket> bookTickets;
+
+    private BookTicketRepository() {
+        bookTickets = new HashSet<>();
+
+        bookTickets.add(new BookTicket.Builder().setBookTicketId("BFW-ZTRYT-213").setMovie("John Wick 4").setCustomer("Daniella Gilbert").setDateTime(LocalDateTime.parse("2024-04-13T18:30")).setSeatNumber("B17").build());
+        bookTickets.add(new BookTicket.Builder().setBookTicketId("SGB-MBWPA-652").setMovie("Fast and Furious 8").setCustomer("Sydney April").setDateTime(LocalDateTime.parse("2024-04-20T20:00")).setSeatNumber("A05").build());
+        bookTickets.add(new BookTicket.Builder().setBookTicketId("XLT-FLBSW-887").setMovie("Lift").setCustomer("Jonathan Ford").setDateTime(LocalDateTime.parse("2024-04-26T20:30")).setSeatNumber("F50").build());
+    }
+
+    public static BookTicketRepository getRepository() {
+        if (repository == null) {
+            repository = new BookTicketRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public BookTicket create(BookTicket bookTicket) {
+        bookTickets.add(bookTicket);
+        return bookTicket;
+    }
+
+    @Override
+    public BookTicket read(String bookTicketId) {
+        for (BookTicket bookTicket : bookTickets) {
+            if (bookTicket.getBookTicketId().equals(bookTicketId)) {
+                return bookTicket;
+            }
+        }
+        return null;
+    }
+    @Override
+    public BookTicket update(BookTicket bookTicket) {
+        return bookTicket;
+    }
+    @Override
+    public void delete(String bookTicketId) {
+        BookTicket bookTicket = read(bookTicketId);
+        if (bookTicket != null) {
+            bookTickets.remove(bookTicket);
+        }
+    }
+    @Override
+    public Set<BookTicket> getAll() {
+        return new HashSet<>(bookTickets);
+    }
+}
